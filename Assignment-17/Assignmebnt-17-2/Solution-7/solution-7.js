330d8d44ed794218b692a2f6ce651befae02f027
Aradhana Yadav <103102710+aradhanayada@users.noreply.github.com>
2023-12-04 14:34:08 +0530

    Add files via upload

---
+++ Assignment-17/Assignmebnt-17-2/Solution-7/solution-7.js
@@ -0,0 +1,4 @@
+//A program in JavaScript to calculate the area of a circle given its radius value of 10. Use appropriate arithmetic operators.
+let radius=10;
+let area =3.14*radius*radius;//here multiplication arthamatic operater is used
+console.log("area of circle="+area);
