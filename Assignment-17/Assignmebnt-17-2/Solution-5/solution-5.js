1941e049db5f2d508cf61417c8efaec1408cd86a
Aradhana Yadav <103102710+aradhanayada@users.noreply.github.com>
2023-12-04 14:28:28 +0530

    Add files via upload

---
+++ Assignment-17/Assignmebnt-17-2/Solution-5/solution-5.js
@@ -0,0 +1,6 @@
+// Write a JavaScript program that calculates the simple interest using the formula Simple interest = (principal * rate * time) / 100.
+let principal=1000;
+let rate=5;
+let time=2;
+let Simple_intrest= (principal*rate*time)/100;
+console.log("Simple intrest="+ Simple_intrest);
