b7df7d0af30e32acb12fba88e34d658ed5cebf6b
Aradhana Yadav <103102710+aradhanayada@users.noreply.github.com>
2023-12-05 13:55:37 +0530

    Add files via upload

---
+++ Assignmant-19/Solution-5/solution-5.js
@@ -0,0 +1,16 @@
+// Use the while loop to print n9mbers from 1 to 25 in ascending and descending order.
+// Ascending order
+console.log("Ascending Order:");
+let numAsc = 1;
+while (numAsc <= 25) {
+    console.log(numAsc);
+    numAsc++;
+}
+
+// Descending order
+console.log("Descending Order:");
+let numDesc = 25;
+while (numDesc >= 1) {
+    console.log(numDesc);
+    numDesc--;
+}
