471bfbb6c29d2a7f18231063028109cb976f33ee
Aradhana Yadav <103102710+aradhanayada@users.noreply.github.com>
2023-12-05 12:51:43 +0530

    Add files via upload

---
+++ Assignment-18/Solution-3/solution-3.js
@@ -0,0 +1,7 @@
+// Use a nested ternary operator to check that a number is positive, negative or zero. 
+// You have to print “positive” if the number is positive and similarly for negative and zero also.
+let number = 5; // You can replace this with any number
+
+let result = (number > 0) ? "positive" : (number < 0) ? "negative" : "zero";
+
+console.log(`The number is ${result}.`);
