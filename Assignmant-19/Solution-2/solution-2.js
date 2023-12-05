4dfa1968e438c5a92742a514397c1b17876b235d
Aradhana Yadav <103102710+aradhanayada@users.noreply.github.com>
2023-12-05 13:49:40 +0530

    Add files via upload

---
+++ Assignmant-19/Solution-2/solution-2.js
@@ -0,0 +1,18 @@
+// A program that grades st9dents based on their marks.
+let marks=70; // marks can be change
+if(marks>=90)
+{
+    console.log("Grade A")
+}
+else if(marks>=70 && marks<90)
+{
+    console.log("Grade B")
+}
+else if(marks>=50 && marks<70)
+{
+    console.log("Grade C")
+}
+else 
+{
+    console.log("Grade D")
+}
