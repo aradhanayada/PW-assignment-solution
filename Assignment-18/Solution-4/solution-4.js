b82a40b0dce661512eb76154a6c3cc4a0cd06cbe
Aradhana Yadav <103102710+aradhanayada@users.noreply.github.com>
2023-12-05 12:53:17 +0530

    Add files via upload

---
+++ Assignment-18/Solution-4/solution-4.js
@@ -0,0 +1,8 @@
+// Use a ternary operator to check that a person is eligible to vote or not by checking his age. 
+// If the age of the person is less than 18 then “You cannot vote” should be logged else “You can vote” should be logged.
+
+let age = 20; // You can replace this with the person's actual age
+
+let eligibility = (age < 18) ? "You cannot vote" : "You can vote";
+
+console.log(eligibility);
