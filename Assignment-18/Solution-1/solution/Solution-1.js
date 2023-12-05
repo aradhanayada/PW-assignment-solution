f801cad97506de9fc809a1d4e79b2fae52943f0d
Aradhana Yadav <103102710+aradhanayada@users.noreply.github.com>
2023-12-05 12:49:28 +0530

    Add files via upload

---
+++ Assignment-18/Solution-1/solution/Solution-1.js
@@ -0,0 +1,8 @@
+// A program to check whether the number is even or odd using the ternary operator.
+let number = 15; // You can replace this with any number
+
+let result = (number % 2 === 0) ? "even" : "odd";
+
+console.log(`The number is ${result}.`);
+
+
